0b5aa306433e1422857ee79c134cf3c02bbb227a
Shaik Surayya Tabassum <shaiksurayyatabassum@gmail.com>
2025-11-28 08:06:23 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,16 @@
+let name="surayya"
+
+console.log(typeof(name))
+
+let age=18
+console.log(typeof(age))
+
+console.log(typeof "true")//gives o/p as string
+
+console.log(typeof true)//boolean
+
+let box;
+console.log(typeof box)//undefined
+
+let b=null
+console.log(typeof b)//object 
