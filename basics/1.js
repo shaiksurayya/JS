2e497c9221bbef57c43ef61490894692421a3776
Shaik Surayya Tabassum <shaiksurayyatabassum@gmail.com>
2025-11-28 08:00:14 +0000

    declaration of variables

---
+++ basics/1.js
@@ -0,0 +1,22 @@
+const id=15;
+let name="surayya"
+var branch="cse"//prefer not use var as variable declaration ,because the issue of block code
+mail="s@gmail.com"//not a good practice 
+console.log(id)
+
+//id=16//cannot re assign to variable which is declared as console
+
+console.log(id)
+
+
+// console.log(id);
+// console.log(name);
+// console.log(branch);
+// console.log(mail);
+//instead of writing console.log multiple times use console.table
+
+console.table([id,name,branch,mail])
+
+
+
+
